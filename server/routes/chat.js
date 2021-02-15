c9c9471fd5875a352b47d183895a5a243ba505a4
JeffCW <changcheewah96@gmail.com>
2021-02-15 20:51:41 +0800

    done store and sync messages in all recipients accordingly

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const auth = require('../middleware/auth')
 const User = require('../models/User');
 
+function arrayEquality(a, b) {
+    if (a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+
+    return a.every((element, index) => {
+        return element === b[index]
+    })
+}
+
 router.get("/getConversation", auth, async (req, res) => {
     console.log("get conversation")
     try {
@@ -20,16 +31,22 @@ router.post('/addConversation', auth, async (req, res) => {
     console.log("add Conversation")
     try {
         const userId = req.user.id
+        const recipients = req.body.recipients
         console.log('userId', userId)
 
         //$elemMatch used to find if the object key is matched inside the array
-        const allChat = await User.findById({ _id: userId }, { conversation: { $elemMatch: { id: req.body.id } } })
-        if (allChat.conversation.length !== 0) {
-            res.json({ error: 'duplicated Id' })
-            return
+        const allChat = await User.findById({ _id: userId })
+
+        for (let conversation of allChat.conversation) {
+            let conversationExist = arrayEquality(conversation.recipients, recipients)
+            if (conversationExist) {
+                res.json({ error: 'duplicated Id' })
+                return
+            }
         }
 
         //setting {new: true} will get the latest docs after update the document
+        // const response = await User.findOneAndUpdate({ $and: [{ _id: userId }, { "conversation.recipients": { $in: req.body.recipients } }] }, { $addToSet: { "conversation.$.messages": [] } }, { new: true })
         const response = await User.findByIdAndUpdate({ _id: userId }, { $addToSet: { conversation: req.body } }, { new: true })
         res.json({ conversations: response.conversation })
 
--- server/server.js
+++ server/server.js
@@ -24,6 +24,17 @@ const io = socketio(server, {
     }
 });
 
+function arrayEquality(a, b) {
+    if (a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+
+    return a.every((element, index) => {
+        return element === b[index]
+    })
+}
+
 io.on('connection', socket => {
     const id = socket.handshake.query.id
     console.log("join id", id)
@@ -34,15 +45,36 @@ io.on('connection', socket => {
         let messageJson = {}
         messageJson.sender = id;
         messageJson.text = text;
+        let messageArr = []
+        messageArr.push(messageJson)
 
         // if the ObjectId is matched and the 'recipients' object inside conversation array is matched with the given array (recipients), push new message. the $ is representing current index of the matched case
-        const response = await User.findOneAndUpdate({ $and: [{ _id: id }, { "conversation.recipients": { $in: recipients } }] }, { $push: { "conversation.$.messages": messageJson } }, { new: true })
+        await User.findOneAndUpdate({ $and: [{ _id: id }, { "conversation.recipients": recipients }] }, { $push: { "conversation.$.messages": messageJson } })
 
-
-        console.log("response", response)
-        recipients.forEach(recipient => {
+        for (let recipient of recipients) {
+            let existingConversation = false
             const newRecipients = recipients.filter(r => r !== recipient)
             newRecipients.push(id)
+            //const response = await User.findOneAndUpdate({ $and: [{ _id: recipient }, { "conversation.recipients": newRecipients }] })
+            const allChat = await User.findById(recipient);
+            for (let conversation of allChat.conversation) {
+                let conversationExist = arrayEquality(conversation.recipients, newRecipients)
+                if (conversationExist) {
+                    existingConversation = true;
+                    break
+                }
+            }
+
+            if (!existingConversation) {
+                let conversationJson = {}
+                conversationJson.recipients = newRecipients;
+                conversationJson.messages = [];
+                await User.findByIdAndUpdate({ _id: recipient }, { $addToSet: { conversation: conversationJson } })
+            }
+
+            await User.findOneAndUpdate({ $and: [{ _id: recipient }, { "conversation.recipients": newRecipients }] }, { $push: { "conversation.$.messages": messageJson } })
+
+            // await User.findOneAndUpdate({ $and: [{ _id: recipient }, { "conversation.recipients": newRecipients }] }, { $push: { "conversation.$.messages": messageJson } }, { new: true })
             console.log("id", id, "newRecipients", newRecipients, "text", text, "recipient", recipient)
 
             // const response = await User.findByIdAndUpdate({ _id: userId }, { $addToSet: { conversation: req.body } }, { new: true })
@@ -50,7 +82,7 @@ io.on('connection', socket => {
             socket.broadcast.to(recipient).emit('receive-message', {
                 recipients: newRecipients, sender: id, text
             })
-        })
+        }
     })
 })
 
