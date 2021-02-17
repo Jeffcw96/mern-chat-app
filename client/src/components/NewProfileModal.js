739457d347b3b827f5edf83cf2aff5aec3074008
JeffCW <changcheewah96@gmail.com>
2021-02-17 20:51:39 +0800

    implementation of file upload to server and s3

--- client/src/components/NewProfileModal.js
+++ client/src/components/NewProfileModal.js
@@ -1,13 +1,32 @@
 import React, { useState } from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
+import axios from 'axios'
+import { getCookie } from '../components/Cookie'
 
+const URL = "http://localhost:5000/"
 export default function NewProfileModal({ closeProfileModal }) {
+
+    async function handleUploadProfile(e) {
+        console.log("e", e)
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+
+        const response = await axios.post(URL + 'profile/uploadProfile', data, {
+            headers: {
+                "Authorization": "Bearer " + getCookie("token")
+            }
+        })
+
+        console.log(response)
+    }
+
     return (
         <>
             <Modal.Header closeButton>Create Chat</Modal.Header>
             <Modal.Body>
                 <Form >
                     <Button type="submit">Start Chating</Button>
+                    <input type="file" onChange={(e) => handleUploadProfile(e)} />
                 </Form>
             </Modal.Body>
         </>
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,6 +2,27 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/User');
+const multer = require('multer');
+const STATICFOLDER = 'public';
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+    accessKeyId: 'AKIAUQDWEJMJVEIOGBGX',
+    secretAccessKey: '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'
+});
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, STATICFOLDER)
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }).single('file')
 
 router.get('/get', auth, async (req, res) => {
     try {
@@ -12,5 +33,44 @@ router.get('/get', auth, async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 })
+
+router.post('/uploadProfile', auth, async (req, res) => {
+    try {
+        upload(req, res, (err) => {
+            if (err) {
+                console.log("err", err)
+                res.sendStatus(500);
+            }
+            const filePath = path.join(__dirname, '../' + STATICFOLDER + '/');
+            const fileDestination = filePath + req.file.filename;
+            console.log("fileDestination", fileDestination);
+            const fileContent = fs.readFileSync(fileDestination);
+
+            // Setting up S3 upload parameters
+            const params = {
+                Bucket: 'connectmeandyou',
+                Key: 'hmmm.jpg', // File name you want to save as in S3
+                Body: fileContent,
+                ContentType: 'image/jpeg',
+                ACL: 'public-read'
+            };
+
+            // Uploading files to the bucket
+            s3.upload(params, function (err, data) {
+                if (err) {
+                    console.log('s3 upload err', err);
+                }
+                console.log(`File uploaded successfully. ${data.Location}`);
+            });
+
+            res.send(req.file);
+        });
+    } catch (error) {
+        console.error('error in uploading profile', error.message)
+        res.status(500).json({ error: error.message })
+    }
+})
+
+
 
 module.exports = router
