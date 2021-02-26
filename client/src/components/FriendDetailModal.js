4adc4780d494e0f1db1cffbc6c4476ac7d2c27e8
JeffCW <changcheewah96@gmail.com>
2021-02-26 20:38:45 +0800

    done edit friend name and log out function

--- client/src/components/FriendDetailModal.js
+++ client/src/components/FriendDetailModal.js
@@ -1,11 +1,55 @@
-import React from 'react'
-import { Modal, Form } from 'react-bootstrap'
+import React, { useState, useRef, useEffect } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
 import userImg from '../static/user.svg'
+import axios from 'axios'
+import { getCookie } from './Cookie'
+import { useContacts } from '../contexts/ContactsProvider'
 
-export default function FriendDetailModal({ friend }) {
+export default function FriendDetailModal({ friend, name, closeFriendModal }) {
+    const saveBtn = useRef();
+    const [friendName, setFriendName] = useState(name)
+    const { setContacts } = useContacts()
+
+    async function updateFriendName(e) {
+        console.log("l;alalal")
+        try {
+            e.preventDefault()
+            let jsonBody = {}
+            jsonBody.id = friend._id;
+            jsonBody.name = friendName;
+
+            const response = await axios.post("social/updateName", jsonBody, {
+                headers: {
+                    "Authorization": "Bearer " + getCookie('token')
+                }
+            })
+
+            if (response.status === 200) {
+                setContacts(response.data.friends)
+                closeFriendModal()
+            }
+
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    function handleFriendName(e) {
+        setFriendName(e.target.value)
+    }
+
+    useEffect(() => {
+        if (friendName !== name) {
+            saveBtn.current.classList.add("d-block");
+        } else {
+            saveBtn.current.classList.remove("d-block");
+        }
+    }, [friendName])
+
+
     return (
         <>
-            <Modal.Header closeButton>{friend.name}</Modal.Header>
+            <Modal.Header closeButton>{name}</Modal.Header>
             <Modal.Body>
                 <div className="profile-pic-container">
                     {
@@ -14,7 +58,7 @@ export default function FriendDetailModal({ friend }) {
                             <img src={userImg} className="avatar" style={{ width: '200px', height: '200px' }} />
                     }
                 </div>
-                <Form>
+                <Form onSubmit={updateFriendName}>
                     <Form.Group className="d-flex justify-content-between align-items-center my-4 px-5">
                         <Form.Label className="w-25">Id:</Form.Label>
                         <Form.Control type="text" className="w-75" disabled value={friend._id} />
@@ -25,11 +69,14 @@ export default function FriendDetailModal({ friend }) {
                     </Form.Group>
                     <Form.Group className="d-flex justify-content-between align-items-center my-4 px-5">
                         <Form.Label className="w-25">Name:</Form.Label>
-                        <Form.Control type="text" className="w-75" value={friend.name} />
+                        <Form.Control type="text" className="w-75" value={friendName} onChange={(e) => handleFriendName(e)} />
                     </Form.Group>
                     <Form.Group className="d-flex justify-content-between align-items-center my-4 px-5">
                         <Form.Label className="w-25">Bio:</Form.Label>
-                        <Form.Control type="text" className="w-75" value={friend.bio} />
+                        <Form.Control type="text" className="w-75" disabled value={friend.bio} />
+                    </Form.Group>
+                    <Form.Group className="d-none text-right my-4 px-5" ref={saveBtn}>
+                        <Button type="submit">Save</Button>
                     </Form.Group>
                 </Form>
             </Modal.Body>
